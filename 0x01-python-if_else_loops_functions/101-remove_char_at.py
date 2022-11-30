889e2e7e1b051563d58896a61ec97cfbc3b8b00d
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-11-29 20:48:55 -0800

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    s = ""
-    for i in range(len(str)):
-        if i != n:
-            s = s + str[i]
+#!/usr/bin/env python3
+remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+print(remove_char_at("Holberton School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
