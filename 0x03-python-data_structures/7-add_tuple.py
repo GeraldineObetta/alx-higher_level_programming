dabe46d4b1ac9448f374d28d27fa09c27f853ce3
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-12-04 06:00:56 -0800

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence):
-    """Returns tuple with len of string and first char"""
-    length = len(sentence)
-    if sentence:
-        first = sentence[0]
-    else:
-        first = None
-    return (length, first)
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    new_tuple = ()
+
+    tuple_1 = tuple_a + (0, 0)
+
+    tuple_2 = tuple_b + (0, 0)
+
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+
+    return new_tuple
