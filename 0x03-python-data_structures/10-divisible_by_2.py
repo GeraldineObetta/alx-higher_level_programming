5c3d9e9d1d1d26a4c67d87c6d74c0c61f6b7cca1
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-12-04 06:07:51 -0800

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,15 +2,16 @@
 
 def divisible_by_2(my_list=[]):
 
-    new_list = []
+    boolist = my_list[:]
 
-    for i in range(len(my_list)):
+    for count, i in enumerate(my_list):
 
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
 
-            new_list.append(True)
+            boolist[count] = True
 
         else:
 
-            new_list.append(False)
+            boolist[count] = False
 
+    return(boolist)
