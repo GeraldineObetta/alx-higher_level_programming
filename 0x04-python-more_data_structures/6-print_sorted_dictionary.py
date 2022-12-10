875b7ba58cc1ff30644ae891cd0fb3a7c0c282e7
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-12-09 17:08:20 -0800

    Task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
